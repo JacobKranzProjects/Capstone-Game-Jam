eb18984861987edf34f119d757c19546fb2f171a
JacobKranzProjects <jacobgkranz@gmail.com>
2025-04-09 11:54:05 -0400

    created basic minesweeper board
    no multiplayer functionality yet, just a minesweeper board with mines and numbers.

---
+++ godot files/game files/cells/cell.gd
@@ -0,0 +1,30 @@
+extends AnimatedSprite2D
+
+var revealed = false
+var number
+var is_mine
+
+var grid_position
+
+func _ready() -> void:
+	update()
+
+
+
+func reveal():
+	if revealed: return
+	revealed = true
+	if number == 0:
+		for cell in get_parent().get_cell_neighbors(grid_position):
+			cell.reveal()
+	update()
+
+
+func update():
+	number = 0
+	for cell in 	get_parent().get_cell_neighbors(grid_position): # get the sum of mines in neighbors
+		if cell.is_mine:
+			number += 1
+	if revealed and !is_mine: frame = number
+	elif revealed and is_mine: frame = 10
+	else: frame = 9 # frame 9 is the un revealed one
