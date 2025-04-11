7bb76a204760d5f28b666ce2fb93d66b66610b25
JacobKranzProjects <jacobgkranz@gmail.com>
2025-04-10 21:58:11 -0400

    made boards be ui nodes

---
+++ godot files/game files/ui_board/scripts/cell manager.gd
@@ -0,0 +1,48 @@
+extends GridContainer
+
+var cell_scene = preload("res://game files/ui_board/scenes/cell.tscn")
+var cell_dict = {}
+
+
+func _ready() -> void:
+	pass
+
+# excludes existing mines and any cells in exclude_cells
+func create_mines(num_mines, exclude_cells): 
+	var valid_cells = []
+	for cell in cell_dict.values():
+		if !cell.is_mine and cell not in exclude_cells:
+			valid_cells.append(cell)
+	if num_mines > valid_cells.size(): num_mines = valid_cells.size() # check to make sure we dont add too many mines
+	valid_cells.shuffle() # randomize valid positions
+	for i in num_mines: # now make a mine in the first 'num_mines' cells
+		valid_cells[i].is_mine = true
+		valid_cells[i].update()
+
+
+
+func create_grid(grid_size):
+	columns = grid_size
+	for i in grid_size:
+		for j in grid_size:
+			var new_cell = cell_scene.instantiate() # Instantiate cell
+			new_cell.grid_position = Vector2(j,i) # set cell grid position
+			cell_dict[new_cell.grid_position] = new_cell # add cell address to dictionary
+			add_child(new_cell) # add the cell as a child
+
+
+func get_cell_neighbors(grid_position):
+	var neighbors = [
+		Vector2(-1,-1),Vector2(0,-1),Vector2(1,-1), # this is a grid of relative neighbor positions
+		Vector2(-1, 0),              Vector2(1, 0), # we can use and edit it to easily get the list of neighbors
+		Vector2(-1, 1),Vector2(0, 1),Vector2(1, 1)
+	]
+	for index in [7,6,5,4,3,2,1,0]: # we loop backwards to not shift indexes when removing null elements
+		neighbors[index] += grid_position # get grid position based on relative one
+		neighbors[index] = cell_dict.get(neighbors[index]) # set element to neighbor address
+		if neighbors[index] == null: neighbors.remove_at(index) # if element is null (borders for example), then we remove it from the list
+	return neighbors # return list
+
+
+func mark(cell_position):
+	cell_dict[cell_position].mark()
--- godot files/game files/ui_board/scripts/cell.gd
+++ godot files/game files/ui_board/scripts/cell.gd
@@ -1,9 +1,10 @@
-extends AnimatedSprite2D
+extends TextureRect
+
 
 var revealed = false
 var is_marked = false
 var number
-var is_mine
+var is_mine = false
 
 var grid_position
 
@@ -16,6 +17,7 @@ func mark():
 	
 
 func reveal():
+	if is_marked: return
 	if revealed: return
 	revealed = true
 	if is_mine:
@@ -32,10 +34,14 @@ func update():
 	for cell in get_parent().get_cell_neighbors(grid_position): # get the sum of mines in neighbors
 		if cell.is_mine:
 			number += 1
-	if revealed and !is_mine: frame = number
-	elif !revealed and !is_marked: frame = 9 # frame 9 is the un revealed one
-	elif revealed and is_mine: frame = 10
-	elif !revealed and is_marked: frame = 11 # frame 11 is the flag
+	
+	if revealed and !is_mine:
+		if 1<=number and number<=8: texture.region = Rect2(((number-1)%4)*64,floor((number-1)/4)*64,64,64) # first two rows of numbers
+		elif number == 0: texture.region = Rect2(0,128,64,64) # empty number
+	elif !revealed and !is_marked: texture.region = Rect2(64,128,64,64) # unknown cell
+	elif revealed and is_mine: texture.region = Rect2(128,128,64,64) # mine
+	elif !revealed and is_marked: texture.region = Rect2(192,128,64,64) # flag
+
 
 func trigger_loss():
 	update()
